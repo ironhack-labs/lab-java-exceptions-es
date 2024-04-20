4f37c37e7bb285bf9564f1735556e00a1791e6c5
isaactaveras <itaveras1989@gmail.com>
2024-04-20 10:32:22 +0200

    add project

---
+++ Lab2.04/src/main/java/org/example/Person.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String occupation;
+
+    public Person(int id, String name, int age, String occupation) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public Person() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age>0) this.age = age;
+        else throw new IllegalArgumentException("Age no puede ser menor que cero");
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public boolean equals(Person person){
+        if (getName().equals(person.getName()) && getAge()==person.getAge() && getOccupation().equals(person.getOccupation())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
